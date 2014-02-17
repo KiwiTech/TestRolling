48814ffec28d4de3581ec916c9cd3350ee00744b
Vijay Deep Gupta <vijayg@kiwitech.com>
2014-02-17 21:23:29 +0530

    CheckCommit

---
+++ TestForRollingBuild/TestSKMasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  TestSKMasterViewController.h
+//  TestForRollingBuild
+//
+//  Created by Vijay Deep Gupta on 2/17/14.
+//  Copyright (c) 2014 Vijay Deep Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class TestSKDetailViewController;
+
+@interface TestSKMasterViewController : UITableViewController
+
+@property (strong, nonatomic) TestSKDetailViewController *detailViewController;
+
+@end
