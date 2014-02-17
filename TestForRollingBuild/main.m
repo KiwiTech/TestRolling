50b5a4048cf3240a0b0060d2757e016a7a2a91de
Vijay Deep Gupta <vijayg@kiwitech.com>
2014-02-17 21:06:12 +0530

    TestCommit

---
+++ TestForRollingBuild/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestForRollingBuild
+//
+//  Created by Vijay Deep Gupta on 2/17/14.
+//  Copyright (c) 2014 Vijay Deep Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TestSKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TestSKAppDelegate class]));
+    }
+}
